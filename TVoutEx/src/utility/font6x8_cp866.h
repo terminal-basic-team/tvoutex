35afc930b8e55383975d017cf88b283a229ccf58
starling13 <starling13@mail.ru>
2020-02-22 22:53:02 +0300

    Add cp866 font

---
+++ TVoutEx/src/utility/font6x8_cp866.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   font6x8_cp866.h
+ * Author: starling13
+ *
+ * Created on 22 февраля 2020 г., 19:02
+ */
+
+#ifndef FONT6X8_CP866_H
+#define FONT6X8_CP866_H
+
+#include <sys/cdefs.h>
+#include <avr/pgmspace.h>
+
+__BEGIN_DECLS
+
+extern const unsigned char Font6x8_cp866[];
+
+__END_DECLS
+
+#endif /* FONT6X8_CP866_H */
+
